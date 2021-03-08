2e998409a33eb6d741e24b04d937751583b6c1d7
Sunyoring <sunhyo67@gmail.com>
2021-03-08 23:39:26 +0900

    Add testOverflow()

--- src/example/variable/Variable.java
+++ src/example/variable/Variable.java
@@ -100,7 +100,14 @@ public class Variable {
 	}
 	public void testOverflow() {
 		//데이터 오버플로우 테스트
-		System.out.println("byte의 최댓값 : " + Byte.MAX_VALUE);
-		System.out.println("byte의 최솟값 : " + Byte.MIN_VALUE);
+		System.out.println("byte의 최대값 : " + Byte.MAX_VALUE);
+		System.out.println("byte의 최소값 : " + Byte.MIN_VALUE);
+		
+//		byte bnum = 128; 저장 가능 범위를 벗어나므로 에러 발생
+		byte bnum = 127;
+		bnum += 1;
+		System.out.println("byte의 최대값 + 1 : " + bnum);
+		bnum -= 1;
+		System.out.println("byte의 최소값 - 1 : " + bnum);
 	}
 }
