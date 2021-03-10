c0f446b6bb25e5f0934996ee1e230a731da4cc0a
Sunyoring <sunhyo67@gmail.com>
2021-03-11 02:27:51 +0900

    method2

--- src/practice/operator/OperatorPractice.java
+++ src/practice/operator/OperatorPractice.java
@@ -1,5 +1,7 @@
 package practice.operator;
 
+import java.util.Scanner;
+
 public class OperatorPractice {
 	public void method1() {
 		//문제 1 출력 값 예측
@@ -15,7 +17,24 @@ public class OperatorPractice {
 		
 		System.out.println("num1 : " + num1);
 		System.out.println("num2 : " + num2);
-		System.out.println("str : " + str);
+		System.out.println("str : " + str);		
+	}
+	public void method2() {
+		//문제 2 정수 2개를 입력 받아 삼항 연산자를 이용하여 "두 수 모두 홀수이다" 또는
+		// "두 수 모두 짝수이다" 또는 "하나는 홀수이고 하나는 짝수이다" 를 판별하여 출력하시오.
 		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("첫 번째 정수 입력 : ");
+		int num1 = sc.nextInt();
+		System.out.print("두 번째 정수 입력 : ");
+		int num2 = sc.nextInt();
+		
+		String result = (num1 % 2==0) && (num2 %2 ==0) ? "두 수 모두 짝수이다" : 
+			(num1 % 2 !=0) && (num2 % 2 !=0) ?
+		"두 수 모두 홀수이다" : "두 수 중 하나는 홀수이고 하나는 짝수이다";		
+//또는  (num1 % 2 ==0) && (num2 % 2 ==0) ? "두 수 모두 짝수" :
+	// (num1 % 2 ==0) || (num2 % 2 ==0) ? "하나는 짝수, 하나는 홀수" : "두 수 모두 홀수"
+	
+		System.out.println(result);
 	}
 }
--- src/practice/operator/Run.java
+++ src/practice/operator/Run.java
@@ -4,7 +4,8 @@ public class Run {
 
 	public static void main(String[] args) {
 		OperatorPractice op = new OperatorPractice();
-		op.method1();
+//		op.method1();
+		op.method2();
 	}
 
 }
