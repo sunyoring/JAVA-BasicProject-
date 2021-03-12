a2111ed0f93c40a134dbd0561dc4c2b83679e89c
Sunyoring <sunhyo67@gmail.com>
2021-03-13 02:19:47 +0900

    testDeepCopy

--- src/example/array/Array.java
+++ src/example/array/Array.java
@@ -63,5 +63,85 @@ public class Array {
 		System.out.println(originArr);
 		System.out.println(copyArr);
 	}
-
+	
+	public void testDeepCopy() {
+		//깊은 복사 
+		int[] originArr = {1,2,3,4,5};
+		
+		//for문 이용
+		int[] copyArr = new int[10];
+		for (int i = 0; i < originArr.length; i++) {
+			 copyArr[i] = originArr[i] ; //순서 주의 앞에 나온것에게 값을 할당
+		} 
+		System.out.println("=====변경 전=====");
+		System.out.println(Arrays.toString(originArr));
+		System.out.println(Arrays.toString(copyArr));
+		
+		originArr[2] = 100;
+		
+		System.out.println("=====변경 후=====");
+		System.out.println(Arrays.toString(originArr));
+		System.out.println(Arrays.toString(copyArr));
+		
+		System.out.println("=====주소 값====="); //참조변수이므로 주소값이 호출됨.
+		System.out.println(originArr);
+		System.out.println(copyArr);
+	}
+	public void testDeepCopy2() {
+		//깊은 복사 - arraycopy()
+		int[] originArr = {1,2,3,4,5};
+		int[] copyArr = new int[10];
+		
+		//src : 원본 배열
+		//srcPos : 원본 배열의 복사할 시작점
+		//dest : 카피 배열
+		//destPs : 카피 배열의 붙여넣기 시작점
+		//length : 원본 배열에서 가져올 길이
+		System.arraycopy(originArr, 0, copyArr, 0, originArr.length);
+		
+		System.out.println("=====변경 전=====");
+		System.out.println(Arrays.toString(originArr));
+		System.out.println(Arrays.toString(copyArr));
+		
+		originArr[2] = 100;
+		
+		System.out.println("=====변경 후=====");
+		System.out.println(Arrays.toString(originArr));
+		System.out.println(Arrays.toString(copyArr));
+		
+		System.out.println("=====주소 값====="); //참조변수이므로 주소값이 호출됨.
+		System.out.println(originArr);
+		System.out.println(copyArr);
+	}
+	public void testDeepCopy3() {
+		//깊은 복사 - Arrays.copyOf()
+		int[] originArr = {1,2,3,4,5};
+		
+		//할당하지 않고 바록 사용
+		//깊은 복사는 가능하지만 배열의 크기는 늘어나게 만들 수 없음
+		int[] copyArr = new int[10];
+		
+		//original : 원본 배열
+		//newLength : 복사할 길이
+		//무조건 원본 배열의 0부터 시작
+		//값을 대입시켜야함
+		copyArr = Arrays.copyOf(originArr, originArr.length);
+		
+		//copyArr은 originArr을 복사하여 새롭게 생긴 객체를 참조하게 되었으므로
+		// 길이가 5.
+		System.out.println("=====변경 전=====");
+		System.out.println(Arrays.toString(originArr));
+		System.out.println(Arrays.toString(copyArr));
+		
+		originArr[2] = 100;
+		
+		System.out.println("=====변경 후=====");
+		System.out.println(Arrays.toString(originArr));
+		System.out.println(Arrays.toString(copyArr));
+		
+		System.out.println("=====주소 값====="); //참조변수이므로 주소값이 호출됨.
+		System.out.println(originArr);
+		System.out.println(copyArr);
+		
+	}
 }
--- src/example/array/Run.java
+++ src/example/array/Run.java
@@ -5,7 +5,10 @@ public class Run {
 	public static void main(String[] args) {
 		Array arr = new Array();
 //		arr.testArray();
-		arr.testShallowCopy();
+//		arr.testShallowCopy();
+//		arr.testDeepCopy();
+//		arr.testDeepCopy2();
+		arr.testDeepCopy3();
 		
 	}
 
