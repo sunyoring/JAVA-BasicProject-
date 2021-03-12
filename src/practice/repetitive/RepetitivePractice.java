b52dbbb305f75922e6d8f85918cff4cb8b411678
Sunyoring <sunhyo67@gmail.com>
2021-03-13 01:11:09 +0900

    method4

--- src/practice/repetitive/RepetitivePractice.java
+++ src/practice/repetitive/RepetitivePractice.java
@@ -67,6 +67,63 @@ public class RepetitivePractice {
 			result += str;
 			System.out.println("결과 : " + result);
 		}
+	}
 
+	public void method4() {
+		// 카페 주문 프로그램
+		Scanner sc = new Scanner(System.in);
+
+		// 메뉴별 주문량
+		int ame = 0;
+		int latte = 0;
+		int vanilla = 0;
+
+		// 무한반복
+		while (true) {
+			// 메뉴 출력
+			System.out.println("====== 메뉴 ======");
+			System.out.println("1.  아메리카노");
+			System.out.println("2.  카페라떼");
+			System.out.println("3.  바닐라라떼");
+			System.out.println("================");
+
+			System.out.print("메뉴 선택 : ");
+			int menu = sc.nextInt();
+			System.out.print("수량 입력 : ");
+			int amount = sc.nextInt();
+
+			switch (menu) {
+			case 1:
+				ame += amount;
+				break;
+			case 2:
+				latte += amount;
+				break;
+			case 3:
+				vanilla += amount;
+				break;
+			default:
+				System.out.println("잘못 선택하셨습니다. 다시 선택해주세요.");
+				continue;
+			}
+
+			System.out.print("추가 주문 하시겠습니까 ?(y/n) : ");
+			char select = sc.next().charAt(0); // 무한반복 루트안에 있기때문에 n일경우만 구현해주면 된다.
+			if (select == 'n' || select == 'N') {
+				System.out.println("================");
+				if (ame != 0) {
+					System.out.println("아메리카노" + ame + "잔 : " + ame * 3500 + "원");
+				}
+				if (latte != 0) {
+					System.out.println("카페라떼" + latte + "잔 : " + latte * 4100 + "원");
+				}
+				if (vanilla != 0) {
+					System.out.println("바닐라라떼" + vanilla + "잔 : " + vanilla * 4300 + "원");
+				}
+				System.out.println("================");
+				System.out.println("총액 : " + (ame * 3500 + latte * 4100 + vanilla * 4300) + "원");
+				break;
+			}
+		}
 	}
 }
--- src/practice/repetitive/Run.java
+++ src/practice/repetitive/Run.java
@@ -6,7 +6,10 @@ public class Run {
 		RepetitivePractice rp = new RepetitivePractice();
 //		rp.method1();
 //		rp.method2();
-		rp.method3();
+//		rp.method3();
+		rp.method4();
+
+	
 	}
 
 }
