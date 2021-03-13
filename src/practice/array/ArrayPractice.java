771b498b80b9e4196c5cd921a0f694a9fac7187a
Sunyoring <sunhyo67@gmail.com>
2021-03-13 23:45:37 +0900

    mothod2

--- src/practice/array/ArrayPractice.java
+++ src/practice/array/ArrayPractice.java
@@ -1,5 +1,6 @@
 package practice.array;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayPractice {
@@ -7,15 +8,15 @@ public class ArrayPractice {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("홀수인 양의 정수 입력 : ");
 		int[] arr = new int[sc.nextInt()];
-		
-		//증가하는 for문
+
+		// 증가하는 for문
 		int num = 1;
-		for(int i = 0; i < arr.length/2; i++) {
+		for (int i = 0; i < arr.length / 2; i++) {
 			arr[i] = num++;
 			System.out.println(arr[i] + " ");
 		}
-		//감소하는 for문
-		for(int i = arr.length/2; i < arr.length; i++) {
+		// 감소하는 for문
+		for (int i = arr.length / 2; i < arr.length; i++) {
 			arr[i] = num--;
 			System.out.println(arr[i] + " ");
 		}
@@ -30,5 +31,42 @@ public class ArrayPractice {
 //		for(int j = (int)(index/2)-1; j>=0 ; j--) {
 //			System.out.print(num1[j]);
 //		}
+	}
+
+	public void method2() {
+	//문제 2 
+	Scanner sc = new Scanner(System.in);
+	System.out.print("배열의 크기 입력 : ");
+	String[] strArr = new String[sc.nextInt()];
+	
+	sc.nextLine();
+	
+	for(int i =0 ; i <strArr.length; i++) {
+		System.out.print(i+1 + "번째 문자열 : ");
+		strArr[i] = sc.nextLine();
+	}
+	while(true) {
+		System.out.println("값을 더 입력하시겠습니까? (y/n)");
+		String select = sc.nextLine();
+		
+		if(select.toUpperCase().equals("Y")){
+			System.out.print("추가할 개수 : ");
+			int add = sc.nextInt();
+			String[] newArr = new String[strArr.length + add];
+			System.arraycopy(strArr, 0, newArr, 0, strArr.length);
+			
+			sc.nextLine();
+			
+			for(int i = strArr.length; i < newArr.length; i++) {
+				System.out.print(i+1 +"번째 문자열 : ");
+				newArr[i] = sc.nextLine();
+			}	
+			strArr = newArr;
+		}else if(select.toUpperCase().equals("N")) {
+			System.out.println(Arrays.toString(strArr));
+			break;
+		}
+	
+	}
 	}
 }
--- src/practice/array/Run.java
+++ src/practice/array/Run.java
@@ -4,7 +4,8 @@ public class Run {
 
 	public static void main(String[] args) {
 		ArrayPractice ap = new ArrayPractice();
-		ap.method1();
+//		ap.method1();
+		ap.method2();
 	}
 
 }
