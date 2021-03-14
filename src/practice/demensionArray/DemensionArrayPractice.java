9e0474bd30f5f77848dc186d47061cd9574f9b66
Sunyoring <sunhyo67@gmail.com>
2021-03-14 18:57:57 +0900

    method2

--- src/practice/demensionArray/DemensionArrayPractice.java
+++ src/practice/demensionArray/DemensionArrayPractice.java
@@ -1,5 +1,6 @@
 package practice.demensionArray;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class DemensionArrayPractice {
@@ -21,6 +22,30 @@ public class DemensionArrayPractice {
 			}
 			System.out.println();
 		}
-
 	}
+	public void method2() {
+		Scanner sc = new Scanner(System.in); 
+		char [][] arr = new char[5][5];
+		while(true) {
+			System.out.print("행 인덱스 입력 >> ");
+			int x = sc.nextInt();
+			System.out.print("열 인덱스 입력 >> ");
+			int y = sc.nextInt();
+			if(x < 0 || x >= 5 || y < 0 || y >= 5) {
+				break;
+			}
+			arr[x][y] = 'X';			
+			System.out.println("\t0\t1\t2\t3\t4");
+			for(int i = 0; i < arr.length; i++) {
+				System.out.print(i+"\t");
+				for(int j =0; j < arr[i].length; j++) {
+					System.out.print(arr[i][j] + "\t");
+				}
+				System.out.println();
+			}
+			System.out.println();
+		}System.out.println("프로그램을 종료합니다.");
+		
+	}
+	
 }
--- src/practice/demensionArray/Run.java
+++ src/practice/demensionArray/Run.java
@@ -4,7 +4,8 @@ public class Run {
 
 	public static void main(String[] args) {
 		DemensionArrayPractice dap = new DemensionArrayPractice();
-		dap.method1();
+//		dap.method1();
+		dap.method2();
 	}
 
 }
