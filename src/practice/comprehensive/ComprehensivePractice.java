6a9120252cfd05cc190d2300b2153cf2bd9a9653
Sunyoring <sunhyo67@gmail.com>
2021-03-16 21:52:12 +0900

    Upgrade Bingo Game

--- src/practice/comprehensive/ComprehensivePractice.java
+++ src/practice/comprehensive/ComprehensivePractice.java
@@ -8,8 +8,8 @@ public class ComprehensivePractice {
 
 		System.out.print("빙고판 크기 지정 : ");
 		int num = sc.nextInt();
-
 		int[] arr = new int[num * num];
+
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = (int) (Math.random() * (num * num)) + 1;
 			// 중복이 발생하면 j--
@@ -56,19 +56,67 @@ public class ComprehensivePractice {
 			}
 
 			// 게임 종료 조건 확인
-			boolean flag = false;
+			// 모든 칸이 0이 되면 게임 종료
+//			boolean flag = false;
+//			for (int i = 0; i < darr.length; i++) {
+//				for (int j = 0; j < darr[i].length; j++) {
+//					if (darr[i][j] != 0) {
+//						flag = true;
+//					}
+//				}				
+//			}
+//			if (flag == false) {
+//				break;
+//			}	
+			// 3빙고 이면 게임 종료
+			int bingoCnt = 0;
+			int crossCnt1 = 0;
+			int crossCnt2 = 0;
+
 			for (int i = 0; i < darr.length; i++) {
+				int colCnt = 0;
+				int rowCnt = 0;
 				for (int j = 0; j < darr[i].length; j++) {
-					if (darr[i][j] != 0) {
-						flag = true;
+					// 가로
+					if (darr[i][j] == 0) {
+						rowCnt++;
 					}
-				}				
+					// 세로
+					if (darr[j][i] == 0) {
+						colCnt++;
+					}
+					//왼쪽에서 오른쪽 아래로 대각선
+					if(i == j && darr[i][j] == 0) {
+						crossCnt1++;
+					}
+					if(i+j == num-1 && darr[i][j] == 0){
+						crossCnt2++;
+					}
+				}
+				//가로 한줄이 모두 0이면 빙고
+				if(rowCnt == num) {
+					bingoCnt++;
+				}
+				if(colCnt == num) {
+					bingoCnt++;
+				}
 			}
-			if (flag == false) {
+			//왼쪽에서 오른쪽 아래로 대각선이 모두 0이면 빙고
+			if(crossCnt1 == num) {
+				bingoCnt++;
+			}
+			//오른쪽에서 왼쪽 아래로 대각선이 모두 0이면 빙고
+			if(crossCnt2 == num) {
+				bingoCnt++;
+			}
+			
+			//빙고가 3개이면 게임 종료
+			if(bingoCnt == 3) {
 				break;
-			}	
+			}
+
 		}
 		System.out.println("빙 고 완 성 !!");
-		System.out.println("=======빙고 게임 종료=======");	 
+		System.out.println("=======빙고 게임 종료=======");
 	}
 }
